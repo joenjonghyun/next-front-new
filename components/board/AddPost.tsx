2428c5e990f592b1bc5d887df04a564e58985beb
joenjonghyun <jjh980814@naver.com>
2022-05-31 17:53:06 +0900

    수정해야함

---
+++ components/board/AddPost.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AddPost(write){
+    return(
+        <>
+        <h1></h1>
+        <h1></h1>
+        <button>등록</button>
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import { Counter } from '@/components/counter/Counter'
 import type { NextPage } from 'next'
-import styles from '@/styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <Counter/>
+    
   )
 }
 
